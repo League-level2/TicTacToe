a2f3337a49d0298d9bf7f9938f57b90bdf80488c
League Student <league@iMac-5.attlocal.net>
2018-03-29 20:30:10 -0700

    slight change

--- src/Controller.java
+++ src/Controller.java
@@ -1,11 +1,10 @@
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class Controller implements MouseListener {
 	Model model;
 	View view;
-	int player = 0;
+	int turn = 0;
 
 	public Controller(Model model, View view) {
 		super();
@@ -27,31 +26,24 @@ public class Controller implements MouseListener {
 	public void mousePressed(MouseEvent e) {
 		int col = (e.getX() / (TicTacToe.WIDTH / 3));
 		int row = (e.getY() / (TicTacToe.HEIGHT / 3));
-	
-		if(model.makeMove(row, col, player + 1)) {
-		
-		player++;
-		player = player % 2;
+		int player = turn + 1;
+		if(model.makeMove(row, col, player)) {
+		turn++;
+		turn = turn % 2;
 		}
 		view.repaint();
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
